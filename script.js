ebc08687afda9786690a14f54d714b78cddf6b1d
zvlzvl <zivile@gmail.com>
2024-11-29 10:54:58 +0200

    data is attached to variables

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const container = document.querySelector('.container');
 fetch("https://api.escuelajs.co/api/v1/users")
 .then(res => res.json())
 .then(data => {
-    console.log(data);
+  new Date().getTime();
+  new Date().getDate();
+
+
     data.forEach(user => {
+        const date = new Date(user.creationAt);
+        const formattedDate = date.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+
         container.innerHTML +=`   <div class="card">
         <img src="${user.avatar}" alt="Avatar">
         <div class="card-content">
@@ -12,7 +22,7 @@ fetch("https://api.escuelajs.co/api/v1/users")
             <p class="role">${user.role}</p>
             <p class="email">${user.email}</p>
         </div>
-        <div class="card-footer">Created At: ${user.creationAt}</div>
+        <div class="card-footer">Created At: ${formattedDate}</div>
     </div>`
     })
 })

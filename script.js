4aa44619b246d4f2d659511693d65b709d41ad10
zvlzvl <zivile@gmail.com>
2024-11-29 10:26:18 +0200

    style and html init

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('.container');
+
+container.innerHTML +=`   <div class="card">
+        <img src="https://i.imgur.com/LDOO4Qs.jpg" alt="Avatar">
+        <div class="card-content">
+            <h2>Jhon</h2>
+            <p class="role">Customer</p>
+            <p class="email">john@mail.com</p>
+            <p class="password">Password: changeme</p>
+            <div class="dates">
+                <p>Created At: 2024-11-29T04:04:22.000Z</p>
+                <p>Updated At: 2024-11-29T04:04:22.000Z</p>
+            </div>
+        </div>
+        <div class="card-footer">User ID: 1</div>
+    </div>`

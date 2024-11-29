412fd23664b68dd1b3e1d26fa9c8eeb8ceb28c18
zvlzvl <zivile@gmail.com>
2024-11-29 10:49:26 +0200

    data is attached to variables

--- script.js
+++ script.js
@@ -4,19 +4,17 @@ fetch("https://api.escuelajs.co/api/v1/users")
 .then(res => res.json())
 .then(data => {
     console.log(data);
+    data.forEach(user => {
+        container.innerHTML +=`   <div class="card">
+        <img src="${user.avatar}" alt="Avatar">
+        <div class="card-content">
+            <h2>${user.name}</h2>
+            <p class="role">${user.role}</p>
+            <p class="email">${user.email}</p>
+        </div>
+        <div class="card-footer">Created At: ${user.creationAt}</div>
+    </div>`
+    })
 })
 
-container.innerHTML +=`   <div class="card">
-        <img src="https://i.imgur.com/LDOO4Qs.jpg" alt="Avatar">
-        <div class="card-content">
-            <h2>Jhon</h2>
-            <p class="role">Customer</p>
-            <p class="email">john@mail.com</p>
-            <p class="password">Password: changeme</p>
-            <div class="dates">
-                <p>Created At: 2024-11-29T04:04:22.000Z</p>
-                <p>Updated At: 2024-11-29T04:04:22.000Z</p>
-            </div>
-        </div>
-        <div class="card-footer">User ID: 1</div>
-    </div>`
+
